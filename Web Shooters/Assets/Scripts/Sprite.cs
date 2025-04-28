54d6a596d3633bf5e2f2dacd607e0f78a5a3da7c
vaibhavyxx <vd5443@rit.edu>
2025-04-27 23:56:17 -0400

    movement works but does not destroy itself after it is off camera

--- Web Shooters/Assets/Scripts/Sprite.cs
+++ Web Shooters/Assets/Scripts/Sprite.cs
@@ -1,16 +1,27 @@
+//Responsible for movement on update
 using UnityEngine;
 
 public class Sprite : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform destroyTransform;    //to destroy the object once it is off camera for rendering purposes
+
+    Rigidbody2D rb;
+    public float speed = -5.0f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();     //Finds rigidbody attached to the gameobject
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(Vector2.up * speed);     //Adds force at a constant speed 
+        Debug.Log(this.transform.position);  //For debugging purposes
+
+        //Did not work
+        //Destroys itself once it is off camera
+        if(rb.position == (Vector2)destroyTransform.position) { 
+            Destroy(this.gameObject);   
+        }
     }
 }
