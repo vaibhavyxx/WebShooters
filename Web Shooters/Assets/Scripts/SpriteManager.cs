4335af13c99f3a8d2c0449f0fae66dc15be4a695
vaibhavyxx <vd5443@rit.edu>
2025-04-29 13:54:10 -0400

    destroy works

--- Web Shooters/Assets/Scripts/SpriteManager.cs
+++ Web Shooters/Assets/Scripts/SpriteManager.cs
@@ -1,20 +1,38 @@
 //Attach this script to spawn manager
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteManager : MonoBehaviour
 {
     public Transform spawnTransform;
+    public Transform destroyTransform;
 
     //Sprites
     public GameObject spawnPrefab;
+    List<GameObject> spawnedItems;
 
     //time
     public float waitTime = 10.0f;
     float currentTime;
 
+    //gives the access for spawning items
+    public List<GameObject> SpawnedItems
+    {
+        get { return spawnedItems; }
+    }
+
+    //removes an item
+    public void RemoveItem(int index)
+    {
+        GameObject item = SpawnedItems[index];
+        Destroy(item);
+        spawnedItems.RemoveAt(index);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spawnedItems = new List<GameObject>();
         currentTime = Time.time;
     }
 
@@ -25,9 +43,10 @@ public class SpriteManager : MonoBehaviour
         if(currentTime>= waitTime)
         {
             currentTime -= waitTime;
-            Debug.Log("spawn transform: " + spawnTransform.position);
             GameObject spawned = Instantiate(spawnPrefab, spawnTransform.position, spawnTransform.rotation);
-            
+            spawnedItems.Add(spawned);
+            Destroy(spawned, 5.0f);         //Long enough to be off camera to destroy itself
         }
+        
     }
 }
