2bd44529a1d085d6109fd7a2de2169149194a04e
vaibhavyxx <vd5443@rit.edu>
2025-04-29 15:56:10 -0400

    game logic web is never hidden

--- Web Shooters/Assets/Scripts/Player.cs
+++ Web Shooters/Assets/Scripts/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject web;
-    //float currentTime;
+    float currentTime;
+    public float waitTime = 1.0f;
+    bool isTapped = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,14 +17,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.inputManager.Tap > 15)
+        /*Debug.Log("From input manager: " + Input.inputManager.Tap);
+
+        if (Input.inputManager.Tap >= 10)
         {
-            Debug.Log("From player class: "+ Input.inputManager.Tap);
+            currentTime += Time.deltaTime;
+            isTapped = true;
+        }
+
+        if(isTapped && currentTime < waitTime)
+        {
             web.SetActive(true);
         }
-        else
+
+        //if it passes over 2 seconds, the object is hidden and is not tapped anymore
+       if(currentTime >= waitTime)
         {
+            currentTime = 0.0f;
             web.SetActive(false);
-        }
+            isTapped=false;
+        }*/
+
     }
 }
