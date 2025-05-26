c84ef60f4240fb1a53900a732f73117322f9982c
vaibhavyxx <vd5443@rit.edu>
2025-05-26 18:45:48 -0400

    game

---
+++ Web Shooters/Assets/Scripts/Scenes.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(int sceneNumber)
+    {
+
+    }
+}
