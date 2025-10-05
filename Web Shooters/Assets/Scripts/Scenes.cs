3844a5a54938d9bfff29d785f4d413ffbf23b1f8
vaibhavyxx <vd5443@rit.edu>
2025-10-05 12:00:17 -0400

    fixed button issues and updated cursor

--- Web Shooters/Assets/Scripts/Scenes.cs
+++ Web Shooters/Assets/Scripts/Scenes.cs
@@ -1,23 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnApplicationPause(bool pause)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //Loads scene
     public void ChangeScene(int sceneNumber)
     {
+        StartCoroutine(LoadYourAsyncScene(sceneNumber));
+    }
 
+    public void Test()
+    {
+        Debug.Log("Hello World");
+    }
+
+    //Loads scene in the background
+    IEnumerator LoadYourAsyncScene(int sceneBuildIndex)
+    {
+        AsyncOperation asycLoad = SceneManager.LoadSceneAsync(sceneBuildIndex);
+
+        while(!asycLoad.isDone)
+        {
+            yield return null;
+        }
+        Debug.Log("Scene should load");
     }
 }
